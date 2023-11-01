0e3159834225731e3fa1018747bd30a4d967fed9
workwindows <yj160521@163.com>
2023-11-01 11:16:41 +0800

    初始化

---
+++ Test/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @date：2023/10/17 10:01
+# @Author：LiuYiJie
+# @file： __init__.py
---
+++ Test/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @date：2023/10/17 10:01
+# @Author：LiuYiJie
+# @file： main
+from typing import Union
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
+
+@app.get('/')
+async def test():
+    return {"Hello": "world"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": f"接口id：{q}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
